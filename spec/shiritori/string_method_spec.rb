09951c7b42f855427012eed496e6c010e46341d4
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-18 11:26:33 +0900

    Update string_method_spec.rb

--- spec/shiritori/string_method_spec.rb
+++ spec/shiritori/string_method_spec.rb
@@ -10,105 +10,70 @@ describe "Shiritori test" do
     let(:word){ "Hello" }
     let(:num){ 100 }
 
-    def check( method, obj = same_object )
-      main.exec_method_chain( method, obj )
-    end
-
-    it '#to_s method' do
-      expect(check("to_s")).to eq [:to_s, object.to_s]
-    end
-
-    it '#inspect method' do
-      expect(check("inspect")).to eq [:inspect, object.inspect]
-    end
-
-    it '#<=> method' do
-      expect(check("<=>('#{object}')")).to eq [:<=>, object.<=>(object)]
-    end
-
-    it '#== method' do
-      expect(check("==('#{object}')")).to eq [:==, object.==(object)]
-    end
-
-    it '#=== method' do
-      expect(check("===('#{object}')")).to eq [:===, object.===(object)]
-    end
-
-    it '#eql? method' do
-      expect(check("eql?('#{object}')")).to eq [:eql?, object.eql?(object)]
-    end
-
-    it '#hash method' do
-      expect(check("hash")).to eq [:hash, object.hash]
-    end
-
-    it '#casecmp method' do
-      expect(check("casecmp('#{object}')")).to eq [:casecmp, object.casecmp(object)]
-    end
-
-    it "#+ method" do
-      expect(check("+('#{word}')")).to eq [:+, object.+(word)]
-    end
-
-    it '#* method' do
-      expect(check("*(3)")).to eq [:*, object.*(3)]
-    end
-
-    it '#% method' do
-      expect(check("%(3)")).to eq [:%, object.%(3)]
-    end
-
-    it '#[] method' do
-      expect(check("[](0)")).to eq [:[], object.[](0)]
-    end
-
-    it '#[]= method' do
-      expect(check("[]=(0, 'P')")).to eq [:[]=, object.[]=(0, 'P')]
-    end
-
-    it '#insert method' do
-      expect(check("insert(0, 'Perl')")).to eq [:insert, object.insert(0, 'Perl')]
-    end
-
-    it '#length method' do
-      expect(check("length")).to eq [:length, object.length]
-    end
-
-    it '#size method' do
-      expect(check("size")).to eq [:size, object.size]
-    end
-
-    it '#bytesize method' do
-      expect(check("bytesize")).to eq [:bytesize, object.bytesize]
-    end
-
-    it '#empty? method' do
-      expect(check("empty?")).to eq [:empty?, object.empty?]
-    end
-
-    it '#=~ method' do
-      expect(check("=~(/R/)")).to eq [:=~, object.=~(/R/)]
-    end
-
-    it '#match method' do
-      expect(check("match(/R/)")).to eq [:match, object.match(/R/)]
-    end
-
-    it '#succ method' do
-      expect(check("succ")).to eq [:succ, object.succ]
-    end
-
-    it '#succ! method' do
-      expect(check("succ!")).to eq [:succ!, object.succ!]
-    end
-
-    it '#next method' do
-      expect(check("next")).to eq [:next, object.next]
-    end
-
-    it '#next! method' do
-      expect(check("next!")).to eq [:next!, object.next!]
-    end
+    
+    it { instance_check("to_s") }
+    it { instance_check("inspect") }
+    it { instance_check("hash") }
+    it { instance_check("<=>", %Q('#{object}')) }
+    it { instance_check("==", %Q('#{object}')) }
+    it { instance_check("===", %Q('#{object}')) }
+    it { instance_check("casecmp", %Q('#{object}')) }
+    it { instance_check("+", %Q('#{word}')) }
+    it { instance_check("*", %Q(3)) }
+    it { instance_check("%", %Q(3)) }
+    it { instance_check("[]", %Q(0)) }
+    it { instance_check("[]=", 0, %Q('P')) }
+    it { instance_check("insert", 0, %Q('Perl')) }
+    it { instance_check("length") }
+    it { instance_check("size") }
+    it { instance_check("bytesize") }
+    it { instance_check("empty?") }
+    it { instance_check("=~", %Q(/R/)) }
+    it { instance_check("match", %Q(/R/)) }
+    it { instance_check("succ") }
+    it { instance_check("succ!") }
+    it { instance_check("next") }
+    it { instance_check("next!") }
+    it { instance_check("index", %Q('R')) }
+    it { instance_check("rindex", %Q('R')) }
+    it { instance_check("replace", %Q('Python')) }
+    it { instance_check("ascii_only?") }
+    it { instance_check("bytes") }
+    it { instance_check("capitalize") }
+    it { instance_check("capitalize!") }
+    it { instance_check("center", %Q(10)) }
+    it { instance_check("chars") }
+    it { instance_check("chomp") }
+    it { instance_check("chomp!") }
+    it { instance_check("chop") }
+    it { instance_check("chop!") }
+    it { instance_check("clear") }
+    it { instance_check("codepoints") }
+    it { instance_check("concat", %Q('Python')) }
+    it { instance_check("count", %Q('R')) }
+    it { instance_check("crypt", %Q('siman')) }
+    it { instance_check("delete", %Q('R')) }
+    it { instance_check("delete!", %Q('R')) }
+    it { instance_check("downcase") }
+    it { instance_check("downcase!") }
+    it { instance_check("dump") }
+    it { instance_check("slice", %Q(0)) }
+    it { instance_check("byteslice", %Q(0)) }
+    it { instance_check("chr") }
+    it { instance_check("each_byte.to_a") }
+    it { instance_check("each_char.to_a") }
+    it { instance_check("each_codepoint.to_a") }
+    it { instance_check("each_line.to_a") }
+    it { instance_check("encode", %Q(Encoding::UTF_8)) }
+    it { instance_check("encode!", %Q(Encoding::UTF_8)) }
+    it { instance_check("encoding") }
+    it { instance_check("end_with?", %Q('y')) }
+    it { instance_check("eql?", %Q('Python')) }
+    it { instance_check("force_encoding", %Q('UTF-8')) }
+    it { instance_check("getbyte", %Q(0)) }
+    it { instance_check("gsub", %Q(/R/), %Q('P')) }
+    it { instance_check("gsub!", %Q(/R/), %Q('P')) }
+    it { instance_check("hex", obj: object_int) }
 
     it '#upto method' do
       result = check("upto('Z')","A")
@@ -116,144 +81,20 @@ describe "Shiritori test" do
       expect(result.last.to_a).to eq 'A'.upto('Z').to_a
     end
 
-    it '#index method' do
-      expect(check("index('R')")).to eq [:index, object.index('R')]
+    it '#include? method' do
+      expect(check("include?('P')")).to eq [:include?, object.include?('P')]
     end
 
-    it '#rindex method' do
-      expect(check("index('R')")).to eq [:index, object.index('R')]
+    it '#intern method' do
+      expect(check("intern")).to eq [:intern, object.intern]
     end
 
-    it '#replace method' do
-      expect(check("replace('Python')")).to eq [:replace, object.replace('Python')]
+    it '#to_sym method' do
+      expect(check("to_sym")).to eq [:to_sym, object.to_sym]
     end
 
-    it '#ascii_only? method' do
-      expect(check("ascii_only?")).to eq [:ascii_only?, object.ascii_only?]
-    end
-
-    it '#bytes method' do
-      expect(check("bytes")).to eq [:bytes, object.bytes]
-    end
-
-    it '#capitalize method' do
-      expect(check("capitalize")).to eq [:capitalize, object.capitalize]
-    end
-
-    it '#capitalize! method' do
-      expect(check("capitalize!")).to eq [:capitalize!, object.capitalize!]
-    end
-
-    it '#center method' do
-      expect(check("center(10)")).to eq [:center, object.center(10)]
-    end
-
-    it '#chars method' do
-      expect(check("chars")).to eq [:chars, object.chars]
-    end
-
-    it '#chomp method' do
-      expect(check("chomp")).to eq [:chomp, object.chomp]
-    end
-
-    it '#chomp! method' do
-      expect(check("chomp!")).to eq [:chomp!, object.chomp!]
-    end
-
-    it '#chop method' do
-      expect(check("chop")).to eq [:chop, object.chop]
-    end
-
-    it '#chop! method' do
-      expect(check("chop!")).to eq [:chop!, object.chop!]
-    end
-
-    it '#clear method' do
-      expect(check("clear")).to eq [:clear, object.clear]
-    end
-
-    it '#codepoints method' do
-      expect(check("codepoints")).to eq [:codepoints, object.codepoints]
-    end
-
-    it '#concat method' do
-      expect(check("concat('Python')")).to eq [:concat, object.concat('Python')]
-    end
-
-    it '#count method' do
-      expect(check("count('R')")).to eq [:count, object.count('R')]
-    end
-
-    it '#crypt method' do
-      expect(check("crypt('siman')")).to eq [:crypt, object.crypt('siman')]
-    end
-
-    it '#delete method' do
-      expect(check("delete('R')")).to eq [:delete, object.delete('R')]
-    end
-
-    it '#delete! method' do
-      expect(check("delete!('R')")).to eq [:delete!, object.delete!('R')]
-    end
-
-    it '#downcase method' do
-      expect(check("downcase")).to eq [:downcase, object.downcase]
-    end
-
-    it '#downcase! method' do
-      expect(check("downcase!")).to eq [:downcase!, object.downcase!]
-    end
-
-    it '#dump method' do
-      expect(check("dump")).to eq [:dump, object.dump]
-    end
-
-    it '#slice method' do
-      expect(check("slice(0)")).to eq [:slice, object.slice(0)]
-    end
-
-    it '#byteslice method' do
-      expect(check("byteslice(0)")).to eq [:byteslice, object.byteslice(0)]
-    end
-
-    it '#chr method' do
-      expect(check("chr")).to eq [:chr, object.chr]
-    end
-
-    it '#each_byte method' do
-      expect(check("each_byte.to_a")).to eq [:each_byte, object.each_byte.to_a]
-    end
-
-    it '#each_char method' do
-      expect(check("each_char.to_a")).to eq [:each_char, object.each_char.to_a]
-    end
-
-    it '#each_codepoint method' do
-      expect(check("each_codepoint.to_a")).to eq [:each_codepoint, object.each_codepoint.to_a]
-    end
-
-    it '#each_line method' do
-      expect(check("each_line.to_a")).to eq [:each_line, object.each_line.to_a]
-    end
-
-    it '#encode method' do
-      expect(check("encode(Encoding::UTF_8)")).to eq [:encode, object.encode(Encoding::UTF_8)]
-    end
-
-    it '#encode! method' do
-      expect(check("encode!(Encoding::UTF_8)")).to eq [:encode!, object.encode!(Encoding::UTF_8)]
-    end
-
-    it '#encoding method' do
-      expect(check("encoding")).to eq [:encoding, object.encoding]
-    end
-
-    it '#end_with? method' do
-      expect(check("end_with?('y')")).to eq [:end_with?, object.end_with?('y')]
-    end
-
-    it '#eql? method' do
-      expect(check("eql?('Python')")).to eq [:eql?, object.eql?('Python')]
+    it '#lines method' do
+      expect(check("lines")).to eq [:lines, object.lines]
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,8 @@ require 'shiritori'
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 
 module Helpers
-  # Replace standard input with faked one StringIO. 
+  METHOD_PATTERN = /[\w|\?|\>|\<|\=|\!|\[|\[|\]|\*|\/|\+|\-|\^|\~|\@|\%|\&|]+/
+
   def fake_stdin(args)
     begin
       $stdin = StringIO.new
@@ -12,6 +13,21 @@ module Helpers
       yield
     ensure
       $stdin = STDIN
+    end
+  end
+
+  def check( method, obj = same_object )
+    main.exec_method_chain( method, obj )
+  end
+
+  def instance_check(ope, *args, obj: object)
+
+    tobj = ( obj.respond_to?(:dup) )? obj.dup : obj
+
+    if args.nil?
+      expect(check(ope)).to eq [ope.to_sym, eval("#{tobj.inspect}.#{ope}")]
+    else
+      expect(check("#{ope}(#{args.join(',')})", obj||object)).to eq [ope.scan(METHOD_PATTERN).first.to_sym, eval("#{tobj.inspect}.#{ope}(#{args.join(',')})")]
     end
   end
 end
