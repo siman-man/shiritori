9f21352942d0cb7e1f142b33be7fe61efc396cd2
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-06-15 20:03:13 +0900

    Update view.rb

--- lib/shiritori/view.rb
+++ lib/shiritori/view.rb
@@ -3,7 +3,7 @@ module Shiritori
     PADDING = 2
 
     def new_line(num = 0)
-      $stdout.puts "\n"*num
+      puts "\n"*num
     end
 
     def welcome_message
@@ -17,11 +17,11 @@ module Shiritori
       chain = "#{@current_chain.join('.')}"
       chain_size = [chain.size+PADDING, 22].max
 
-      $stdout.puts "+#{'-'*chain_size}+"
-      $stdout.puts "|#{"Current method chain".center(chain_size)}|"
-      $stdout.puts "+#{'-'*chain_size}+"
-      $stdout.puts "|#{@current_chain.join('.').center(chain_size)}|"
-      $stdout.puts "+#{'-'*chain_size}+"
+      puts "+#{'-'*chain_size}+"
+      puts "|#{"Current method chain".center(chain_size)}|"
+      puts "+#{'-'*chain_size}+"
+      puts "|#{@current_chain.join('.').center(chain_size)}|"
+      puts "+#{'-'*chain_size}+"
 
       cls = "#{@current_class}"
       obj = "#{@current_object.to_ss}"
@@ -29,11 +29,11 @@ module Shiritori
       obj_size = ["#{@current_object}".size, 14].max+PADDING
 
       new_line
-      $stdout.puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
-      $stdout.puts "|#{"Current Class".center(cls_size)}|#{"Current Object".center(obj_size)}|"
-      $stdout.puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
-      $stdout.puts "|#{cls.center(cls_size)}|#{obj.center(obj_size)}|"
-      $stdout.puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
+      puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
+      puts "|#{"Current Class".center(cls_size)}|#{"Current Object".center(obj_size)}|"
+      puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
+      puts "|#{cls.center(cls_size)}|#{obj.center(obj_size)}|"
+      puts "+#{'-'*(cls_size)}+#{'-'*(obj_size)}+"
       new_line
       puts "Current Chain Count: #{@chain_count}"
     end
