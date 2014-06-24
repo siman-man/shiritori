7049c6577424dcb03f18b288e47d978ad8d8d68b
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-06-24 22:54:42 +0900

    Add sized_queue.rb

---
+++ lib/shiritori/yaml/sized_queue.rb
@@ -0,0 +1,11 @@
+:SizedQueue:
+  :singleton_method:
+    :new: [SizedQueue]
+  :instance_method:
+    :push: [SizedQueue]
+    :<<: [SizedQueue]
+    :enq: [SizedQueue]
+    :pop: [Object]
+    :shift: [Object]
+    :deq: [Object]
+    :max: [Fixnum]
