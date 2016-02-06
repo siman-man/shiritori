a99af6d604a3c1ebbf6738d6ac740bde182df3e6
siman-man <k128585@ie.u-ryukyu.ac.jp>
2016-02-06 15:24:28 +0900

    bug fix

--- lib/shiritori/search_method.rb
+++ lib/shiritori/search_method.rb
@@ -11,15 +11,11 @@ module Shiritori
       @method_list - UNUSE_METHOD
     end
 
-    def singleton(klass)
-      class << klass; self end
-    end
-
     def scan_method(klass = BasicObject)
       @check_list[klass] = true
       @method_list |= klass.instance_methods
 
-      ObjectSpace.each_object(singleton(klass)) do |subclass|
+      ObjectSpace.each_object(Class) do |subclass|
         scan_method(subclass) if klass != subclass && @check_list[subclass].nil?
       end
     end
