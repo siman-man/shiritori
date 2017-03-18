5dced86dcaa2d55b92190390a0a9c4a54986cf06
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-03-19 01:13:37 +0900

    rename method and simplify code

--- lib/shiritori/search_method.rb
+++ lib/shiritori/search_method.rb
@@ -4,16 +4,13 @@ module Shiritori
 
     def get_all_method
       @check_list = {}
-      @method_list = scan_methods
+      @method_list = get_all_methods
 
       @method_list - UNUSE_METHOD
     end
 
-    def scan_methods(klass = BasicObject)
-      klasses = ObjectSpace.each_object(Class).to_a
-      klasses |= klasses.map(&:singleton_class)
-
-      klasses.map(&:instance_methods).inject(&:|)
+    def get_all_methods
+      ObjectSpace.each_object(Module).map(&:instance_methods).inject(&:|)
     end
   end
 end
